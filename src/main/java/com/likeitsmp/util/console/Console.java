c6790e40a186a0cb0799702c24b71af1112aa456
LikeSauR <popadynetsalexander@gmail.com>
2024-07-20 18:41:58 +0300

    implemented Console

--- src/main/java/com/likeitsmp/util/console/Console.java
+++ src/main/java/com/likeitsmp/util/console/Console.java
@@ -1,9 +1,65 @@
 package com.likeitsmp.util.console;
 
+import org.bukkit.Bukkit;
+
+import com.google.common.base.Preconditions;
+
 public final class Console
 {
+    public static void log(Object message)
+    {
+        var logger = Bukkit.getLogger();
+        var messageLines = (" "+message).split("\n");
+        for (var messageLine : messageLines)
+        {
+            logger.info(messageLine);
+        }
+    }
+
     public static void logWarning(Object message)
     {
-        throw new UnsupportedOperationException("Unimplemented method 'logWarning'");
+        var logger = Bukkit.getLogger();
+        var messageLines = (" "+message).split("\n");
+        for (var messageLine : messageLines)
+        {
+            logger.warning(messageLine);
+        }
+    }
+
+    public static void logError(Object message)
+    {
+        var logger = Bukkit.getLogger();
+        var messageLines = (" "+message).split("\n");
+        for (var messageLine : messageLines)
+        {
+            logger.severe(messageLine);
+        }
+    }
+
+    public static void debugRunAll(Runnable... tasks)
+    {
+        for (var i = 0; i < tasks.length; i++)
+        {
+            var task = tasks[i];
+            debugRun(task);
+        }
+    }
+
+    public static void debugRun(Runnable task)
+    {
+        Preconditions.checkArgument(task != null, "task must not be null");
+
+        try
+        {
+            task.run();
+        }
+        catch (Exception exception)
+        {
+            logError(exception);
+        }
+    }
+
+    private Console()
+    {
     }
 }
