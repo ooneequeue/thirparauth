a72829e0d2e80e6a2974e3ef801471ca31b5a9b8
LikeSauR <popadynetsalexander@gmail.com>
2024-07-20 18:28:31 +0300

    utilized UserAuthProcessInitiator
    > outlined UserAuthProcessInitiator

--- src/main/java/com/likeitsmp/thirparauth/ThirparauthPlugin.java
+++ src/main/java/com/likeitsmp/thirparauth/ThirparauthPlugin.java
@@ -2,6 +2,13 @@ package com.likeitsmp.thirparauth;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.likeitsmp.thirparauth.processes.UserAuthProcessInitiator;
+
 public final class ThirparauthPlugin extends JavaPlugin
 {
+    @Override
+    public void onEnable()
+    {
+        new UserAuthProcessInitiator();
+    }
 }
---
+++ src/main/java/com/likeitsmp/thirparauth/processes/UserAuthProcessInitiator.java
@@ -0,0 +1,9 @@
+package com.likeitsmp.thirparauth.processes;
+
+public final class UserAuthProcessInitiator
+{
+    public UserAuthProcessInitiator()
+    {
+        throw new UnsupportedOperationException("Unimplemented method 'new UserAuthProcessInitiator'");
+    }
+}
